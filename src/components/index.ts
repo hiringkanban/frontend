2b427a0716efb506225e34707e151f7ed80f310c
Bilal <bilal89dif@gmail.com>
2022-12-15 15:30:40 +0100

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Paragraph } from './Atoms/Paragraph';
 export { default as Heading } from './Atoms/Heading';
 export { default as Menu } from './Molecules/menu';
 export { default as Button } from './Atoms/Button';
+export 
