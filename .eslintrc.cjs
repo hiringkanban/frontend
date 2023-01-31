3ed1ce1ac600893a89457f1f72ec1a6bc6a1e5eb
Bilal <bilal89dif@gmail.com>
2023-01-31 08:46:12 +0100

    update rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,5 +35,15 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
